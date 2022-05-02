4cf08f17358b93a2fe27513aaeadba0651a4a820
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-02 17:12:33 +0000

    added code

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 Kaitlyn Ip All rights reserved
+//
+// Created by: Kaitlyn Ip
+// Created on: Apr 2022
+// This file contains xxx
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-5-04-JS/sw.js", {
+    scope: "/ICS2O-Unit-5-04-JS/",
+  })
+}
+
+/**
+ * Input
+ */
+function myButtonClicked() {
+  var day = document.getElementById("day").checked
+  var age = document.getElementById("age").checked
+
+  // process
+  if (day == "Tuesday" || day == "Thursday") || (age > 12 && age < 21) {
+      document.getElementById("answer").innerHTML= "You're eligible for student pricing."
+  } else {
+      document.getElementById("answer").innerHTML= "You must pay regular price."
+  }
+}
