f87d3f1b8234085f8c7a5d7267c670989f6e054d
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-02 17:27:21 +0000

    fixed code

--- js/script.js
+++ js/script.js
@@ -23,7 +23,7 @@ function myButtonClicked() {
   var age = document.getElementById("age").checked
 
   // process
-  if (day == "tuesday" || day == "thursday") || (age > 12 && age < 21) {
+  if (day == tuesday || day == thursday) || (age > 12 && age < 21)) {
       document.getElementById("answer").innerHTML= "You're eligible for student pricing."
   } else {
       document.getElementById("answer").innerHTML= "You must pay regular price."
