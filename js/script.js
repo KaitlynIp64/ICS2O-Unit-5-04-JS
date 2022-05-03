7e2a0b1e195fed4966c931a2c381f11f34177684
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-03 16:35:19 +0000

    fixed code

--- js/script.js
+++ js/script.js
@@ -19,13 +19,15 @@ if (navigator.serviceWorker) {
  * Input
  */
 function myButtonClicked() {
-  var day = document.getElementById("day").checked
-  var age = document.getElementById("age").checked
+  const day = document.getElementById("day").checked;
+  const age = document.getElementById("age").checked;
 
   // process
-  if ((day == tuesday || day == thursday) || (age > 12 && age < 21)) {
-      document.getElementById("answer").innerHTML= "You're eligible for student pricing."
+  if ((day == "tuesday" || day == "thursday") || (age > 12 && age < 21)) {
+      document.getElementById("answer").innerHTML= 
+        "You're eligible for student pricing.";
   } else {
-      document.getElementById("answer").innerHTML= "You must pay regular price."
+      document.getElementById("answer").innerHTML= 
+        "You must pay regular price.";
   }
 }
